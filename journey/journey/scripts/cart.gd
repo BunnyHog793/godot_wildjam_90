b322ddf9e2d2f7651abe4c91ff97c84e7f7d562f
bunnyhog <bunnyhhog294@gmail.com>
2026-02-17 00:33:39 -0500

    working

--- journey/journey/scripts/cart.gd
+++ journey/journey/scripts/cart.gd
@@ -8,9 +8,12 @@ class_name cart
 @export var wood : int = 5
 @export var rock : int = 5
 
-@export var speed : int = 1
+@export var speed : int = 100
 @export var gravity : int = 10
 @export var jump_hight : int = 500
+@export var number_of_jumps : int = 2
+
+var jump_number = 0
 
 func _ready() -> void:
 	GlobalSignals.connect("rock_collected", add_rock)
@@ -18,12 +21,15 @@ func _ready() -> void:
 	
 	
 func _physics_process(_delta: float) -> void:
-	velocity.x += speed
+	velocity.x = speed + rock
+	#print(velocity.x)
 	velocity.y += gravity
 	move_and_slide()
+	
+	# reset jump count
+	if is_on_floor():
+		jump_number = 0
 
-	
-	
 func die():
 	pass
 	
@@ -33,8 +39,9 @@ func upgrade():
 	
 	
 func jump():
-	if is_on_floor():
+	if jump_number < number_of_jumps:
 		velocity.y = -jump_hight
+		jump_number += 1
 	else:
 		pass
 	
@@ -49,7 +56,7 @@ func add_wood (new_wood : int) -> void:
 
 func add_rock(new_coal : int) -> void:
 	rock += new_coal
-	print(rock)
+	#print(rock)
 
 
 func _on_button_button_down() -> void:
